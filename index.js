83e78a0b0e81430073351ec2cb9835785b3c23c5
edezuraud <eddy.dezuraud@gmail.com>
2023-10-05 22:01:38 +0200

    Set cors for xpostpreview.com only

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ import puppeteer from 'puppeteer';
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://www.xpostpreview.com/');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 app.get('/checkTwitterId/:userId', async (req, res) => {
     const userId = req.params.userId;
     try {
