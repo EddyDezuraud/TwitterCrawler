552a0a8874536de123888e8c16a6b769807d9de8
edezuraud <eddy.dezuraud@gmail.com>
2023-10-05 23:34:59 +0200

    Back to initial system

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import puppeteer from 'puppeteer-core'
-import chromium from 'chrome-aws-lambda'
+import puppeteer from 'puppeteer';
 
 
 const app = express();
@@ -12,26 +11,18 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-});
+  });
 
 app.get('/checkTwitterId/:userId', async (req, res) => {
     const userId = req.params.userId;
-    let browser = null;
-
     try {
-
-        browser = await puppeteer.launch({
-            args: [
-              ...chromium.args,
-              // Add any additional args here if needed
-            ],
-            executablePath: await chromium.executablePath,
-            headless: true,
-          });
-
-
-        // const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch();
         const page = await browser.newPage();
+
+        await page.setUserAgent(
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+        )
+
 
         await page.goto('https://twitter.com/' + userId);
 
@@ -45,13 +36,12 @@ app.get('/checkTwitterId/:userId', async (req, res) => {
         const name = userProfileJson.author.givenName;
         const img = userProfileJson.author.image.thumbnailUrl;
 
+        await browser.close();
 
-        res.status(200).json({ name, img });
+        res.status(200).json({name, img});
 
     } catch (error) {
         res.status(500).json({ error: error.message });
-    } finally {
-        await browser.close();
     }
 
 
